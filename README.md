9d1e117ec9895752457740d5f1b393e2b7c305ae
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-03 01:56:46 +0300

    Apollo

---
+++ README.md
@@ -0,0 +1,1 @@
+# ski-day-counter
---
+++ index.js
@@ -0,0 +1,49 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+const typeDefs = gql`
+
+type SkiDay {
+    id: ID!
+    date: String!
+    mountain: String!
+    conditions: Conditions
+}
+
+enum Conditions {
+    POWDER
+    HEAVY
+    ICE
+    THIN
+}
+
+type Query {
+    totalDays: Int!
+    allDays: [SkiDay!]!
+}
+
+input AddDayInput {
+    date: String!
+    mountain: String!
+    conditions: Conditions
+}
+
+type Mutation {
+    addDay(input: AddDayInput!): SkiDay!
+    removeDay(id: ID!): SkiDay!
+}
+`;
+
+// const resolvers = {
+
+// }
+
+const server = new ApolloServer({
+    typeDefs,
+    mocks: true
+});
+
+server
+    .listen()
+    .then(({ url }) =>
+        console.log(`Server running at ${url}`)
+    );
