33008d9b1e17a991d47a60871e8beaf6a0cb2921
moroz-art-dev <moroz.art.dev@gmail.com>
2022-07-04 17:55:34 +0300

    Finished

--- index.js
+++ index.js
@@ -1,49 +1,110 @@
-const { ApolloServer, gql } = require("apollo-server");
+const { createServer } = require("http");
+const { execute, subscribe } = require("graphql");
+const { SubscriptionServer } = require("subscriptions-transport-ws");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { MockList } = require("@graphql-tools/mock");
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+const express = require("express");
 
-const typeDefs = gql`
+(async function () {
+  const typeDefs = gql`
+    scalar Date
 
-type SkiDay {
-    id: ID!
-    date: String!
-    mountain: String!
-    conditions: Conditions
-}
+    """
+    An object that describes the characteristics of a ski day
+    """
+    type SkiDay {
+      "A ski day's unique identifier"
+      id: ID!
+      date: Date!
+      mountain: String!
+      conditions: Conditions
+    }
 
-enum Conditions {
-    POWDER
-    HEAVY
-    ICE
-    THIN
-}
+    enum Conditions {
+      POWDER
+      HEAVY
+      ICE
+      THIN
+    }
 
-type Query {
-    totalDays: Int!
-    allDays: [SkiDay!]!
-}
+    type Query {
+      totalDays: Int!
+      allDays: [SkiDay!]!
+    }
 
-input AddDayInput {
-    date: String!
-    mountain: String!
-    conditions: Conditions
-}
+    input AddDayInput {
+      date: Date!
+      mountain: String!
+      conditions: Conditions
+    }
 
-type Mutation {
-    addDay(input: AddDayInput!): SkiDay!
-    removeDay(id: ID!): SkiDay!
-}
-`;
+    type RemoveDayPayload {
+      day: SkiDay!
+      remove: Boolean
+      totalBefore: Int
+      totalAfter: Int
+    }
 
-// const resolvers = {
+    type Mutation {
+      addDay(input: AddDayInput!): SkiDay!
+      removeDay(id: ID!): RemoveDayPayload!
+    }
 
-// }
+    type Subscription {
+      newDay: SkiDay!
+    }
+  `;
 
-const server = new ApolloServer({
+  // const resolvers = {
+
+  // }
+
+  const mocks = {
+    Date: () => "1/2/2025",
+    String: () => "Cool date",
+    Query: () => ({
+      allDays: () => new MockList([1, 15]),
+    }),
+  };
+
+  const app = express();
+  const httpServer = createServer(app);
+
+  const schema = makeExecutableSchema({
     typeDefs,
-    mocks: true
-});
+    mocks,
+    // resolvers,
+  });
 
-server
-    .listen()
-    .then(({ url }) =>
-        console.log(`Server running at ${url}`)
-    );
+  const subscriptionServer = SubscriptionServer.create(
+    { schema, execute, subscribe },
+    { server: httpServer, path: "/graphql" }
+  );
+
+  const server = new ApolloServer({
+    schema,
+    plugins: [
+      {
+        async serverWillStart() {
+          return {
+            async drainServer() {
+              subscriptionServer.close();
+            },
+          };
+        },
+      },
+      ApolloServerPluginLandingPageGraphQLPlayground(),
+    ],
+  });
+  await server.start();
+  server.applyMiddleware({ app });
+
+  const PORT = 4000;
+  httpServer.listen(PORT, () =>
+    console.log(`Server is now running on http://localhost:${PORT}/graphql`)
+  );
+})();
